6c1616101a0c3a7c030904adb67f6e90bb8ab880
Jesuisla <MurBe@Muratas-MacBook-Air.local>
2014-04-26 11:42:40 -0400

    Final assignment

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	
+	$("#mobNav").click(function() {			/* Mobile nav toggle */
+		$("#navi").slideToggle();
+	});
+
+});
+
+$(document).ready(function() {
+			$("#twitter").hover(function(){
+				$("#twitter").animate({
+					opacity:"0.4"
+				});
+			});
+
+		});
+
+$(document).ready(function() {
+			$("#facebook").hover(function(){
+				$("#facebook").animate({
+					opacity:"0.4"
+				});
+			});
+
+		});
